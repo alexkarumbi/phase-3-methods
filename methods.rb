7f2ca0b3ed9b415912ea1eeff29bddae51b35174
AlexKarumbi <alex.muhia@moringaschool.com>
2022-10-25 21:37:03 +0300

    commit tree df0688ea0deba45974e47b5f976a075f5ecfa751

--- methods.rb
+++ methods.rb
@@ -1,1 +1,13 @@
-# Your code here!
+def greet_programmer 
+    puts "Hello,programmer!"
+end
+
+def greet (name)
+    puts "Hello,#{name}!"
+end
+
+def greet (name)
+    puts "Hello,#{name}!"
+end
+
+def add (a,)
